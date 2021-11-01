0613c8c848be44a3e4f79c7bbba611dcb6a65eda
Karel Cervicek <cervicekkarel@gmail.com>
2021-11-01 14:10:56 +0100

    adds

--- evaluation/Evaluator.py
+++ evaluation/Evaluator.py
@@ -7,7 +7,7 @@ from evaluation.operations import get_high, get_low
 import matplotlib.pyplot as plt
 
 
-def get_profit_long(x, position_params):
+def get_profit(x, position_params):
     if x[0] == x[1]:
         if x[2] > position_params['tp']:
             return position_params['tp'] - position_params['spread']
@@ -23,10 +23,14 @@ def get_profit_long(x, position_params):
 def get_eval_fces(type, position_params):
     if type == 'long':
         return (lambda high_diff: get_high(high_diff, position_params['tp']),
-        lambda low_diff: get_low(low_diff, position_params['sl']))
+        lambda low_diff: get_low(low_diff, position_params['sl']),
+        lambda high, low: (high, low),
+        lambda open_price, open_tail_price : (open_tail_price - open_price)*position_params['points'])
     else:
         return (lambda low_diff: get_low(low_diff, position_params['tp']),
-        lambda high_diff: get_high(high_diff, position_params['sl']))
+        lambda high_diff: get_high(high_diff, position_params['sl']),
+        lambda high, low: (low, high),
+        lambda open_price, open_tail_price : (open_price - open_tail_price)*position_params['points'])
 
 def add_profit_along_time(pair_timeseries, position_params):
     points = position_params['points']
@@ -35,10 +39,8 @@ def add_profit_along_time(pair_timeseries, position_params):
     low_price_win = sliding_window_view(pair_timeseries['Low'].values, window_shape = observe)[:-2]
     open_price = pair_timeseries['Open'].values[:-observe-1]
     open_tail_price = pair_timeseries['Open'].values[observe+1:]
-    open_tail_price = (open_tail_price - open_price)*points
 
     open_price = open_price.reshape(open_price.shape[0],1)
-    open_tail_price = open_tail_price.reshape(open_tail_price.shape[0],1)
 
     open_to_high = np.concatenate(
             [open_price, high_price_win], axis=1)
@@ -47,10 +49,15 @@ def add_profit_along_time(pair_timeseries, position_params):
     open_to_high = np.apply_along_axis(lambda x: (x - x[0])*points, 1, open_to_high)
     open_to_low = np.apply_along_axis(lambda x: (x - x[0])*points, 1, open_to_low)
 
-    get_tp, get_sl = get_eval_fces('long', position_params)
-    tp_arr = np.apply_along_axis(lambda x: get_tp(x), 1, open_to_high).reshape(open_to_high.shape[0],1)
-    sl_arr = np.apply_along_axis(lambda x: get_sl(x), 1, open_to_low).reshape(open_to_low.shape[0],1)
-    profit = np.apply_along_axis(lambda x: get_profit_long(x, position_params), 1, np.concatenate([tp_arr, sl_arr, open_tail_price], axis=1))
+    get_tp, get_sl, get_correct_diffs, get_open_tail_diffs = get_eval_fces('long', position_params)
+    diffs_for_tp, diffs_for_sl = get_correct_diffs(open_to_high, open_to_low)
+    
+    open_tail_price = (open_tail_price - open_price)*points
+    open_tail_price = open_tail_price.reshape(open_tail_price.shape[0],1)
+
+    tp_arr = np.apply_along_axis(lambda x: get_tp(x), 1, diffs_for_tp).reshape(diffs_for_tp.shape[0],1)
+    sl_arr = np.apply_along_axis(lambda x: get_sl(x), 1, diffs_for_sl).reshape(diffs_for_sl.shape[0],1)
+    profit = np.apply_along_axis(lambda x: get_profit(x, position_params), 1, np.concatenate([tp_arr, sl_arr, open_tail_price], axis=1))
 class Evaluator:
     def __init__(self, pair_timeseries, position_params):
         self.pair_timeseries = add_profit_along_time(pair_timeseries, position_params)
